db1a16632b2caf38ed562045456317ade6740444
Henrique Lobo <loboh67@gmail.com>
2023-03-01 23:50:29 +0000

    Cenas iniciais

---
+++ programa.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public class programa {
+    public static void main(String[] args) throws IOException {
+        Downloader.download("http://www.google.com");
+    }
+}
