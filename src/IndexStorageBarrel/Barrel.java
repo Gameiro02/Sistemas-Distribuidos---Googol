d00da69d07a5f30e727036c5905e70d724d5a385
Gonçalo Gameiro <gmeiro2002@gmail.com>
2023-03-13 15:19:06 +0000

    Nao me mates lobo

---
+++ src/IndexStorageBarrel/Barrel.java
@@ -0,0 +1,12 @@
+package src.IndexStorageBarrel;
+
+// Java class to store the index in barrels
+// The class need to import from a file the current links of the barrel
+// The page also need to store in a file the current links of the barrel
+
+public class Barrel {
+    public void writeToFile() {
+        // Write the index to a file
+
+    }
+}
