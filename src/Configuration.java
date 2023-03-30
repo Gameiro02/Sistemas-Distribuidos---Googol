592bdb08f3e8dadecb9edf71bd8ce109a961b578
Henrique Lobo <loboh67@gmail.com>
2023-03-30 15:46:45 +0100

    dsa

--- src/Configuration.java
+++ src/Configuration.java
@@ -4,9 +4,9 @@ public class Configuration {
     public static final String CREDENTIALS_FILE = "credentials.bin";
 
     public boolean DEBUG = true;
-    public static boolean COLD_START = true;
-    public static boolean AUTO_FAIL_BARRELS = false;
-    public static boolean AUTO_FAIL_DOWNLOADERS = false;
+    public static boolean COLD_START = true; // Experimental
+    public static boolean AUTO_FAIL_BARRELS = true;
+    public static boolean AUTO_FAIL_DOWNLOADERS = true;
 
     // TCP ports
     public static final int PORT_A = 8080;
@@ -15,7 +15,7 @@ public class Configuration {
     public static final int MULTICAST_PORT = 4321;
     public static final String MULTICAST_ADDRESS = "224.3.2.1";
 
-    public static final int NUM_DOWNLOADERS = 2;
+    public static final int NUM_DOWNLOADERS = 6;
     public static final int NUM_BARRELS = 4; // Even number
 
     public static final int CONTEXT_SIZE = 15;
